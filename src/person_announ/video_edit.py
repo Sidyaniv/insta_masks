9eaea88bf8c81732c22e2a554b0b2bbd726dad59
asilyanov <asilyanov@npomis.ru>
2024-10-30 12:02:20 +0300

    Добавил основу для DNN face detection. Реализовал главные функции модуля в main.py()

--- src/person_announ/video_edit.py
+++ src/person_announ/video_edit.py
@@ -4,24 +4,29 @@ from image_edit import ImageEditing
 from enums import *
 
 
-def get_video(vid_type: str, video_path: str = ''):
+def get_video_window(width: int, height: int):
+    cap = cv2.VideoCapture(0)
+    # устанавливаем размеры выводимого видео
+    # propid==3 - width, propid==4 - height
+    cap.set(3, width)
+    cap.set(4, height)
+    return cap
+
+
+def display_video(vid_type: str, video_path: str = ''):
     cap = None
     if vid_type == 'video':
         cap = cv2.VideoCapture(video_path)
     elif vid_type == 'camera':
-        cap = cv2.VideoCapture(0)
-        # устанавливаем размеры выводимого видео
-        # propid==3 - width, propid==4 - height
-        cap.set(3, 640)
-        cap.set(4, 480)
+        cap = get_video_window(640, 480)
 
     while True:
         # первая переменная указывает на успешность захвата кадра, а вторая - сам кадр
         success, img = cap.read()
-        cadr = ImageEditing(image=img)
-        cadr.bgr2gray()
-        cadr.gray2binary()
-        cv2.imshow('video', cadr.img)
+        frame = ImageEditing(image=img)
+        frame.detect_preparation()
+    
+        cv2.imshow('video', frame.img)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
